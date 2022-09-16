ec035af31055887a5014952af433a9a8ef9e1e06
inflexiont <inflexiontcare@gmail.com>
2022-09-16 16:42:48 +0300

    implement-infinite-scroll-in-conversation

--- server/server.js
+++ server/server.js
@@ -2,29 +2,33 @@ const auth = require("json-server-auth");
 const jsonServer = require("json-server");
 const express = require("express");
 const http = require("http");
-const { response } = require("express");
 
 const app = express();
 const server = http.createServer(app);
 const io = require("socket.io")(server);
+
 global.io = io;
 
 const router = jsonServer.router("db.json");
+
 // response middleware
 router.render = (req, res) => {
-  const path = req.path;
-  const method = req.method;
-  if (
-    path.includes("/conversations") &&
-    (method === "POST" || method === "PATCH")
-  ) {
-    // emit socket event
-    io.emit("conversation", {
-      data: res.locals.data,
-    });
-  }
-  res.json(response.locals.data);
+    const path = req.path;
+    const method = req.method;
+
+    if (
+        path.includes("/conversations") &&
+        (method === "POST" || method === "PATCH")
+    ) {
+        // emit socket event
+        io.emit("conversation", {
+            data: res.locals.data,
+        });
+    }
+
+    res.json(res.locals.data);
 };
+
 const middlewares = jsonServer.defaults();
 const port = process.env.PORT || 9000;
 
@@ -34,9 +38,9 @@ app.db = router.db;
 app.use(middlewares);
 
 const rules = auth.rewriter({
-  users: 640,
-  conversations: 660,
-  messages: 660,
+    users: 640,
+    conversations: 660,
+    messages: 660,
 });
 
 app.use(rules);
--- src/features/messages/messagesApi.js
+++ src/features/messages/messagesApi.js
@@ -1,20 +1,19 @@
 import { apiSlice } from "../api/apiSlice";
 
 export const messagesApi = apiSlice.injectEndpoints({
-  endpoints: (builder) => ({
-    getMessages: builder.query({
-      query: (id) =>
-        `/messages?conversationId=${id}&_sort=timestamp&_order=desc&_page=1&_limit=${process.env.REACT_APP_MESSAGES_PER_PAGE}`,
-      // listen message socket
+    endpoints: (builder) => ({
+        getMessages: builder.query({
+            query: (id) =>
+                `/messages?conversationId=${id}&_sort=timestamp&_order=desc&_page=1&_limit=${process.env.REACT_APP_MESSAGES_PER_PAGE}`,
+        }),
+        addMessage: builder.mutation({
+            query: (data) => ({
+                url: "/messages",
+                method: "POST",
+                body: data,
+            }),
+        }),
     }),
-    addMessage: builder.mutation({
-      query: (data) => ({
-        url: "/messages",
-        method: "POST",
-        body: data,
-      }),
-    }),
-  }),
 });
 
 export const { useGetMessagesQuery, useAddMessageMutation } = messagesApi;
